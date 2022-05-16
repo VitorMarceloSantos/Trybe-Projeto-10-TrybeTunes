4c14de0f5cb38b5dfb387756bb8547203cb03763
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-05-16 19:49:22 -0300

    Exercicio 03

--- Exercicios/Fundamentos/Bloco-04/Exercicio-4.3/script.js
+++ Exercicios/Fundamentos/Bloco-04/Exercicio-4.3/script.js
@@ -17,3 +17,20 @@ for(let i = (palavra.length - 1); i >= 0; i-=1){ //O contador(i) irá receber o 
 inverso += palavra[i];
 }
 console.log(`O iverso da paralavra: ${palavra} Equivale a: ${inverso};`);
+
+//Exercício - 03
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior = [0];
+let menor = array[0]; //ambas as variáveis vão receber o valor contido na posição [0];
+
+for (let i = 0; i < array.length; i+=1){
+  if (array[i].length > maior.length){ //vai testar o tamanho das strings
+  maior = array[i];
+  }
+  if(array[i].length < menor.length){ //vai testar o tamanho das strings
+  menor = array[i];
+  }
+}
+
+console.log(`A maior string é: ${maior}`);
+console.log(`A menor string é: ${menor}`);
