4e4329724320b1febd8732fa425f3635ee53b908
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-05-16 19:55:52 -0300

    Exercicio Bonus 05

--- Exercicios/Fundamentos/Bloco-04/Exercicio-4.3/script.js
+++ Exercicios/Fundamentos/Bloco-04/Exercicio-4.3/script.js
@@ -105,3 +105,68 @@ for (let i = 0; i < quantAst3; i += 1)
   console.log(tabelaAst3);
   contador += 1; // o contador sempre vai receber mais um, a cada execução
 } 
+
+//Exercício - 04
+
+let quantAst4 = 11;
+let numInteiro = Math.trunc(quantAst4 / 2); //pega o valor arrendodado para baixo do meio da variavel quantAst4
+
+let direita = numInteiro;
+let esquerda = numInteiro;
+
+for (let i = 0; i < quantAst4; i += 2)
+{
+let tabelaAst4 ="";
+for (let e = esquerda; e > 0; e -= 1)
+{ 
+tabelaAst4 += " "; //vai somando os espaços vazios na esquerda
+}
+for (let c = 0; c <= i; c += 1){ 
+tabelaAst4 += "*"; // vai somando os asteristicos
+}
+for (let d = direita; d > 0; d -= 1)
+{ 
+tabelaAst4 += " "; //vai somando os espaços vazios na direita
+}
+esquerda -= 1;
+direita -= 1;
+console.log(tabelaAst4); //imprimi a linha
+} 
+
+//Exercício - 05
+
+let quantAst5 = 5;
+let numInteiro5 = Math.trunc(quantAst5 / 2); //pega o valor arrendodado para baixo do meio da variavel quantAst4
+
+let direita5 = numInteiro5;
+let esquerda5 = numInteiro5;
+
+for (let i = 0; i < quantAst5; i += 2)
+{
+  let tabelaAst5 ="";
+  for (let e = esquerda5; e > 0; e -= 1)
+  { 
+    tabelaAst5 += " "; //vai somando os espaços vazios na esquerda
+  }
+  let contadorInterno = i;
+  for (let c = 0; c <= i; c += 1){ 
+    if(i != quantAst5 -1){ //Vai verificar se está atualizando a ultima linha
+      if((contadorInterno == i) || (contadorInterno == 0)){
+      tabelaAst5 += "*"; // vai somando os asteristicos
+      contadorInterno -=1; //faz o decremento
+      }else{
+      tabelaAst5 += " "; //vai somando os espaços vazios na direita
+      contadorInterno -=1; //faz o decremento
+      }
+    }else{
+      tabelaAst5 += "*"; // vai somando os asteristicos
+    }
+  }
+  for (let d = direita5; d > 0; d -= 1)
+  { 
+    tabelaAst5 += " "; //vai somando os espaços vazios na direita
+  }
+  esquerda5 -= 1;
+  direita5 -= 1;
+  console.log(tabelaAst5); //imprimi a linha
+} 
