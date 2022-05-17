3128c6c5c21e27da176704fbe707af1e687283b9
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-05-17 17:42:25 -0300

    Exercicio 01

---
+++ Exercicios/Fundamentos/Bloco-04/script.js
@@ -0,0 +1,8 @@
+//Exercício 01
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+console.log(`Bem vinda, ${info["personagem"]}.`);
