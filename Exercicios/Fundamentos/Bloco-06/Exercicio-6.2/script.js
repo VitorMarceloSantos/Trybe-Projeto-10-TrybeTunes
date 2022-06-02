4cbc8f61b843fb0146df25fef89f30bbe3ae2162
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-06-01 21:24:25 -0300

    Btn Modal

--- Exercicios/Fundamentos/Bloco-06/Exercicio-6.2/script.js
+++ Exercicios/Fundamentos/Bloco-06/Exercicio-6.2/script.js
@@ -1,14 +1,18 @@
-const enviar = document.querySelector('#submit');
+// const enviar = document.querySelector('#submit');
 
-function buscarFormulario(event) {
-  const nome = document.querySelector('#name-txt').value;
+// function buscarFormulario(event) {
+//   const nome = document.querySelector('#name-txt').value;
 
-  event.preventDefault();
-  console.log(nome);
-}
+//   event.preventDefault();
+//   console.log(nome);
+// }
 
-enviar.addEventListener('click', buscarFormulario)
+// enviar.addEventListener('click', buscarFormulario)
 
-// const myModal = new bootstrap.Modal(document.getElementById('myModal'), () => {
-//   modalTeste.show(); // apresenta na tela
-// })
+const btnModal = document.querySelector('#btn-modal');
+const myModal = new bootstrap.Modal(document.getElementById('myModal'));
+
+btnModal.addEventListener('click', () => {
+  myModal.show(); // aparece na tela(pois seu display padrão é none)
+})
+
