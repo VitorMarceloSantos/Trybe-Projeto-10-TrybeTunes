933557e8a5cb562dfe61edfbd013b03f3cf655cc
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-06-18 16:12:09 -0300

    Exericio 4

--- Exercicios/Fundamentos/Bloco-07/Exericio-7.3/functionsEncodeDecode.test.js
+++ Exercicios/Fundamentos/Bloco-07/Exericio-7.3/functionsEncodeDecode.test.js
@@ -17,3 +17,11 @@ test('(a, e, i, o, u) equals (1, 2, 3, 4, 5)', () => {
 test('[1,2,3,4,5] equals (a, e, i, o, u)', () => {
  expect(decode('1,2,3,4,5')).toBe('a,e,i,o,u');
 });
+
+test('(b,c,d,f,g) equals (b,c,d,f,g)', () => {
+  expect(encode('b,c,d,f,g')).toBe('b,c,d,f,g');
+});
+
+test('(6,7,8,9,10) equals (6,7,8,9,10)', () => {
+  expect('(6,7,8,9,10)').toBe('(6,7,8,9,10)');
+});
