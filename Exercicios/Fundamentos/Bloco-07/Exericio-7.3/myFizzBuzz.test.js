ad3216ca4629f64466f44369ee2fd89a8f20060b
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-06-18 15:20:37 -0300

    Exericio 3

--- Exercicios/Fundamentos/Bloco-07/Exericio-7.3/myFizzBuzz.test.js
+++ Exercicios/Fundamentos/Bloco-07/Exericio-7.3/myFizzBuzz.test.js
@@ -4,3 +4,7 @@ const myFizzBuzz = require('./myFizzBuzz');
 test('(15) é divisivel por (3, 5)', () => {
   expect(myFizzBuzz(15)).toBe('fizzbuzz');
 });
+
+test('(6) é divisivel por (6)', () => {
+  expect(myFizzBuzz(6)).toBe('fizz');
+});
