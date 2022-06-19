e1614f9cf16e70d750ae2cfd9a0e1faeeb25f245
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-06-19 11:11:56 -0300

    Exericio 2

--- Academia Lógica/Testes - Jest/script.js
+++ Academia Lógica/Testes - Jest/script.js
@@ -1,16 +1,21 @@
 // Exericios da Academia de Lógica - Testes
 
 const createState = (state, inicials, capital, population) => {
-  try {
+  // try {
     if (!state || !inicials || !capital || !population) {
       throw new Error('Preencha todos os campos para criar sua cidade!');
     }
     return `Bem-vindo ao estado ${state}-${inicials}, nossa capital ${capital} possui ${population} pessoas`;
-  } catch (error) {
-    return error.message;
-  }
+  // } catch (error) {
+  //   return error.message;
+  // }
 };
-//console.log(createState('Caduverso', 'CV', 'Caduversolandia', 137921852));
 
+const finalSpeed = (tempo) => {
+  if (!tempo || typeof tempo !== 'number') return 0;
+  const gravidade = 9.8;
+  return tempo * gravidade;
+};
 
-module.exports = createState;
+module.exports = {createState, finalSpeed};
+
--- Academia Lógica/Testes - Jest/script.test.js
+++ Academia Lógica/Testes - Jest/script.test.js
@@ -1,12 +1,18 @@
 // Exericios da Academia de Lógica - Testes
 
-const createState = require('./script');
+const {createState, finalSpeed} = require('./script');
 
 describe('Crie o nome, sigla, capital e população do seu Estado', () => {
   it('Retorne a string `Bem-vindo ao estado Caduverso-CV, nossa capital Caduversolandia possui 137921852 pessoas` quando passamos `Caduverso`, `CV`, `Caduversolandia` e `137921852`', () => {
     expect(createState(`Caduverso`, `CV`, `Caduversolandia`,`137921852`)).toBe(`Bem-vindo ao estado Caduverso-CV, nossa capital Caduversolandia possui 137921852 pessoas`);
   });
-  // it('Retorne `Preencha todos os campos para criar sua cidade!` caso algum parâmetro não seja passado para a função', () => {
-  //   fail('Remova essa linha e escreva seu teste aqui');
-  // });
+  it('Retorne `Preencha todos os campos para criar sua cidade!` caso algum parâmetro não seja passado para a função', () => {
+    expect(() => {createState()}).toThrow();
+  });
+});
+
+describe('Testando a funçao finalSpeed', () => {
+  test('Retorna 49 caso a função seja chamanda com o argumento 5finalSpeed(5)', () => {
+    expect(finalSpeed(5)).toBe(49);
+  })
 });
