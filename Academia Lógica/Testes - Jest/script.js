e4f55379af5cab8bba2dcab0e7a84cee852256f5
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-06-19 10:14:51 -0300

    Exericio 1

---
+++ Academia Lógica/Testes - Jest/script.js
@@ -0,0 +1,16 @@
+// Exericios da Academia de Lógica - Testes
+
+const createState = (state, inicials, capital, population) => {
+  try {
+    if (!state || !inicials || !capital || !population) {
+      throw new Error('Preencha todos os campos para criar sua cidade!');
+    }
+    return `Bem-vindo ao estado ${state}-${inicials}, nossa capital ${capital} possui ${population} pessoas`;
+  } catch (error) {
+    return error.message;
+  }
+};
+//console.log(createState('Caduverso', 'CV', 'Caduversolandia', 137921852));
+
+
+module.exports = createState;
---
+++ Academia Lógica/Testes - Jest/script.test.js
@@ -0,0 +1,12 @@
+// Exericios da Academia de Lógica - Testes
+
+const createState = require('./script');
+
+describe('Crie o nome, sigla, capital e população do seu Estado', () => {
+  it('Retorne a string `Bem-vindo ao estado Caduverso-CV, nossa capital Caduversolandia possui 137921852 pessoas` quando passamos `Caduverso`, `CV`, `Caduversolandia` e `137921852`', () => {
+    expect(createState(`Caduverso`, `CV`, `Caduversolandia`,`137921852`)).toBe(`Bem-vindo ao estado Caduverso-CV, nossa capital Caduversolandia possui 137921852 pessoas`);
+  });
+  // it('Retorne `Preencha todos os campos para criar sua cidade!` caso algum parâmetro não seja passado para a função', () => {
+  //   fail('Remova essa linha e escreva seu teste aqui');
+  // });
+});
