d19315942d2fd25ca1c3fc2df003c7ce943bd160
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-06-23 19:16:54 -0300

    Exercicio 1

---
+++ Exercicios/Fundamentos/Bloco-08/Exercicio-8.2/script.js
@@ -0,0 +1,79 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+//Utilizando HOF
+
+const functGeral = (array, callback) => {
+  return callback(array);
+}
+
+// Exercicio - 01
+//  Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
+
+const functFind = (livros) => { //HOF
+  return livros.find((livro) => { // Callback
+    return (livro.author.birthYear === 1947); // retorna o elemento
+  }).author.name; // o find irá retornar um elemento, e utiliza o author.name após o find(pega esse elemento retornado pelo find).
+}
+
+ console.log(functGeral(books, functFind));
