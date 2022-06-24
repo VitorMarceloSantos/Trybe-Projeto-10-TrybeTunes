f32bccd02227d8171e28cb88784d6069dceae7b9
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-06-24 11:37:03 -0300

    Exercicio 4

--- Exercicios/Fundamentos/Bloco-08/Exercicio-8.2/script.js
+++ Exercicios/Fundamentos/Bloco-08/Exercicio-8.2/script.js
@@ -76,4 +76,54 @@ const functFind = (livros) => { //HOF
   }).author.name; // o find irá retornar um elemento, e utiliza o author.name após o find(pega esse elemento retornado pelo find).
 }
 
- console.log(functGeral(books, functFind));
+ console.log(`O primeiro autor que nasceu em 1947 é: ${functGeral(books, functFind)}`);
+
+ // Exercício - 02
+ // Retorne o nome do livro de menor nome.
+
+ const funcForEach = (livros) => {
+  let menor = livros[0].name; // vair armazenar o nome do menor livro, recebe como comparacao o primeiro livro
+   livros.forEach((livro) => {
+    if (livro.name.length < menor.length) {
+      menor = livro.name;
+    }
+  });
+  return menor;
+ }
+ console.log(`O menor título é: ${functGeral(books, funcForEach)}`);
+
+ // Exercício - 03
+ // Encontre o primeiro livro cujo nome possui 26 caracteres.
+
+ const funcForEach26 = (livros) => { // Obs: cada função deverá ter seu retorno
+  try{
+    livros.forEach((livro) => {
+      if (livro.name.length === 26) {
+        throw livro.name; // lançando o elemento para o catch
+      }
+    });
+  } 
+  catch(e){ // e - vai receber como parametro o que foi lançado por throw
+    return e; // retorno da função funcForEach26
+  }
+}
+ console.log(`O título com 26 caracteres é: ${functGeral(books, funcForEach26)}`);
+
+// Exercício - 03
+// Encontre o primeiro livro cujo nome possui 26 caracteres.
+
+ const funcFind = (livros) => {
+  return livros.find((livro) => {
+    return livro.name.length === 26; // retorna um array de 1 elemento
+  });
+}
+ console.log(`O título com 26 caracteres é: ${functGeral(books, funcFind).name}`); // ao retornar o elemento, tem como imprimir suas chaves e valor.
+
+ // Exercicio - 04
+ // Ordene os livros por data de lançamento em ordem decrescente.
+
+ const funcSort = (livros) => { // altera a ordem no array principal books.
+  return livros.sort((a, b) => a.releaseYear - b.releaseYear); 
+  // A lógica é a seguinte: a função recebe, da sort, todos os elementos do array, em pares (elemento1, elemento2), e vai comparando-os. O formato é meuArray.sort((elemento1, elemento2) => /* logica da função */). Ou seja: para o array [1, 2, 3, 4], a função receberá (2, 1), (3, 2), (4, 3) como parâmetros e ordenará o array com base em seu resultado. Se a operação de elemento1 com elemento2 der resultado negativo, elemento1 vai para trás. Caso contrário, vai para frente, para, de forma decrescente, só inverter elemento1 - elemento2 para elemento2 - elemento1.
+ }
+ console.log(functGeral(books, funcSort));
