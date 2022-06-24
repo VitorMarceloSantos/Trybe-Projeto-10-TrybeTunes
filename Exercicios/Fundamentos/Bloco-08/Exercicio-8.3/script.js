67b2f60fc963900ece3e15cb36483d499af0eda7
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-06-24 19:20:27 -0300

    Exercicio 2

--- Exercicios/Fundamentos/Bloco-08/Exercicio-8.3/script.js
+++ Exercicios/Fundamentos/Bloco-08/Exercicio-8.3/script.js
@@ -72,9 +72,22 @@ const functGeral = (array, callback) => {
 // Exercicio - 01
 // Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA.
 
-const functMap = (livros) => {
+// const functMap = (livros) => {
+//   return livros.map((livro) => {
+//     return `${livro.name} - ${livro.genre} - ${livro.author.name}`;
+//   })
+// }
+// console.log(functGeral(books, functMap));
+
+// Exercicio - 02
+//  Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+
+const functMapSort = (livros) => {
   return livros.map((livro) => {
-    return `${livro.name} - ${livro.genre} - ${livro.author.name}`;
-  })
+    let objModelo = {};
+    objModelo.author = livro.author.name;
+    objModelo.age = (livro.releaseYear - livro.author.birthYear); // idade na data do lançamento do livro
+    return objModelo;
+  }).sort((a,b) => a.age - b.age); // utilizando o sort com a saída do map
 }
-console.log(functGeral(books, functMap));
+console.log(functGeral(books, functMapSort));
