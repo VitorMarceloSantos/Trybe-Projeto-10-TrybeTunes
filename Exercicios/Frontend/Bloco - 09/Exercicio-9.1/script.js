966615347091666dccdf34aaa974855cbcbe1380
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-07-07 18:20:39 -0300

    Frontend

---
+++ Exercicios/Frontend/Bloco - 09/Exercicio-9.1/script.js
@@ -0,0 +1,161 @@
+// Exercício - 01
+
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+// const mars = {
+//   name: "Mars",
+//   distanceFromSun: {
+//     value: 227900000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// const venus = {
+//   name: "Venus",
+//   distanceFromSun: {
+//     value: 108200000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// const jupiter = {
+//   name: "Jupiter",
+//   distanceFromSun: {
+//     value: 778500000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// console.log(planetDistanceFromSun(mars)); // A
+// console.log(planetDistanceFromSun(venus)); // B
+// console.log(planetDistanceFromSun(jupiter)); // C
+
+// Exercicio - 02
+
+// const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+//   `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+// const mars = {
+//   name: "Mars",
+//   distanceFromSun: {
+//     value: 227900000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// const venus = {
+//   name: "Venus",
+//   distanceFromSun: {
+//     value: 108200000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// const jupiter = {
+//   name: "Jupiter",
+//   distanceFromSun: {
+//     value: 778500000,
+//     measurementUnit: "kilometers",
+//   },
+// };
+
+// console.log(planetDistanceFromSun(mars)); // A
+// setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // C
+// setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // B
+
+// Exercício - 03
+
+// const getPlanet = () => {
+//   const mars = {
+//     name: "Mars",
+//     distanceFromSun: {
+//       value: 227900000,
+//       measurementUnit: "kilometers",
+//     },
+//   };
+//   console.log("Returned planet: ", mars);
+// };
+// setTimeout(() => {
+//   getPlanet(); // imprime Marte depois de 4 segundos
+// }, 4000);
+
+// Exercício - 04
+
+// const messageDelay = () => Math.floor(Math.random() * 5000);
+
+// const getMarsTemperature = () => {
+//   const maxTemperature = 58;
+//   return Math.floor(Math.random() * maxTemperature);
+// };
+
+// const sendMarsTemperature = (() => {
+//   setTimeout(() => {
+//     console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`);
+//   }, messageDelay())
+// }); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+// sendMarsTemperature();
+
+// Exercicio - 05
+
+// const messageDelay = () => Math.floor(Math.random() * 5000);
+
+// const getMarsTemperature = () => {
+//   const maxTemperature = 58;
+//   return Math.floor(Math.random() * maxTemperature);
+// };
+
+// const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+// const temperatureInFahrenheit = (temperature) =>
+//   console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+// const greet = (temperature) =>
+//   console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+// const sendMarsTemperature = ((callback) => {
+//   callback(getMarsTemperature()); // Função callback, passando a função getMarsTemperature como parâmetro
+// });
+
+// setTimeout(() => {
+//   sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+//   setTimeout(() => {
+//     sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
+//   }, messageDelay())
+// }, messageDelay())
+
+// Exercício - 06
+
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+}
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const handleError = (errorReason) =>
+  console.log(`Error getting temperature: ${errorReason}`);
+
+const sendMarsTemperature = (sucesso, erro) => {
+  const currentTemperature = getMarsTemperature();
+  const messageSuccessfullySent = Math.random() <= 0.6; // taxa de sucessso de 60% ao enviar a mensagem
+  setTimeout(() => {
+      if(messageSuccessfullySent) onSuccess(currentTemperature)
+      else onError('Robot is busy');
+  }, messageDelay());
+}
+
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError);
