84b1a864c7410d96d7298f1ec4a49e0f4ccada22
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-08-26 12:13:41 -0300

    Cart

--- Projetos/Projeto Modificados/Frontend Online Store/src/pages/Carrinho.jsx
+++ Projetos/Projeto Modificados/Frontend Online Store/src/pages/Carrinho.jsx
@@ -5,6 +5,14 @@ import { Link } from 'react-router-dom';
 import '../styles/CartCss.css';
 
 class Carrinho extends React.Component {
+  // constructor() {
+  //   super();
+
+  //   this.state = {
+  //     total: '',
+  //   };
+  // }
+
   componentDidMount() {
     if (localStorage.length > 0) {
       const { cartItemsStateUpdate } = this.props;
@@ -22,85 +30,94 @@ class Carrinho extends React.Component {
   render() {
     const { CartItemsList } = this.props;
     const verification = CartItemsList.length > 0;
+    const arrayProducts = [];
     return (
-      <div>
+      <div className="container-cart-global">
         {
           verification ? (
-            <div className="container-cart-products">
-              {
-                CartItemsList.map((item, i) => (
-                  <section key={ `${item.title}${i}` }>
-                    <div className="container-line-product">
-                      <img
-                        src={ item.thumbnail }
-                        alt={ item.title }
-                      />
-                      <div className="container-cart-center">
-                        <h1
-                          data-testid="shopping-cart-product-name"
-                          className="cart-title"
-                        >
-                          { item.title }
-                        </h1>
-                        <div className="container-amount">
-                          <button
-                            data-testid="product-decrease-quantity"
-                            type="button"
-                            name={ item.id }
-                            value="decrease"
-                            onClick={ this.handleOnChange }
+            <div>
+              <div className="container-cart-products">
+                {
+                  CartItemsList.map((item, i) => (
+                    <section key={ `${item.title}${i}` }>
+                      <div className="container-line-product">
+                        <div className="array-products">
+                          {`# 0${arrayProducts.push({
+                            price: item.price, quant: item.quantidade })}`}
+                        </div>
+                        <img
+                          src={ item.thumbnail }
+                          alt={ item.title }
+                        />
+                        <div className="container-cart-center">
+                          <h1
+                            data-testid="shopping-cart-product-name"
+                            className="cart-title"
                           >
-                            {/* Utiliza as tags id e title, para passar os paramentros para a função ao clicar no icone(<i>) */}
-                            <i
-                              className="fa-solid fa-minus"
-                              id={ item.id }
-                              title="decrease"
-                            />
-                          </button>
-                          <p
-                            data-testid="shopping-cart-product-quantity"
-                          >
-                            { item.quantidade }
-                          </p>
-                          <button
-                            data-testid="product-increase-quantity"
-                            type="button"
-                            name={ item.id }
-                            value="increase"
-                            onClick={ this.handleOnChange }
-                          >
-                            <i
-                              className="fa-solid fa-plus"
-                              id={ item.id }
-                              title="increase"
-                            />
-                          </button>
-                          <div className="button-delete">
+                            { item.title }
+                          </h1>
+                          <div className="container-amount">
                             <button
-                              data-testid="remove-product"
+                              data-testid="product-decrease-quantity"
                               type="button"
                               name={ item.id }
-                              value="remove"
+                              value="decrease"
+                              onClick={ this.handleOnChange }
+                            >
+                              {/* Utiliza as tags id e title, para passar os paramentros para a função ao clicar no icone(<i>) */}
+                              <i
+                                className="fa-solid fa-minus"
+                                id={ item.id }
+                                title="decrease"
+                              />
+                            </button>
+                            <p
+                              data-testid="shopping-cart-product-quantity"
+                            >
+                              { item.quantidade }
+                            </p>
+                            <button
+                              data-testid="product-increase-quantity"
+                              type="button"
+                              name={ item.id }
+                              value="increase"
                               onClick={ this.handleOnChange }
                             >
                               <i
-                                className="fa-solid fa-trash"
+                                className="fa-solid fa-plus"
                                 id={ item.id }
-                                title="remove"
+                                title="increase"
                               />
                             </button>
+                            <div className="button-delete">
+                              <button
+                                data-testid="remove-product"
+                                type="button"
+                                name={ item.id }
+                                value="remove"
+                                onClick={ this.handleOnChange }
+                              >
+                                <i
+                                  className="fa-solid fa-trash"
+                                  id={ item.id }
+                                  title="remove"
+                                />
+                              </button>
+                            </div>
                           </div>
                         </div>
+
+                        <h2 className="cart-price">
+                          {`R$ ${item.price.toLocaleString('pt-BR',
+                            { minimumFractionDigits: 2 })}` }
+                        </h2>
                       </div>
-
-                      <h2 className="cart-price">
-                        {`R$ ${item.price.toLocaleString('pt-BR',
-                          { minimumFractionDigits: 2 })}` }
-                      </h2>
-                    </div>
-
-                  </section>))
-              }
+                    </section>))
+                }
+              </div>
+              <div className="container-total-products">
+                {console.log(arrayProducts)}
+              </div>
             </div>
           )
             : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
