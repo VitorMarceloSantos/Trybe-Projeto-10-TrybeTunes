ea03d6f3ae410c8708c16d9e018f4195715f99ba
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-08-28 10:57:38 -0300

    Checkout

--- Projetos/Projeto Modificados/Frontend Online Store/src/pages/Checkout.jsx
+++ Projetos/Projeto Modificados/Frontend Online Store/src/pages/Checkout.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes, { shape } from 'prop-types';
-import '../Checkout.css';
-// requisito 12
+import '../styles/Checkout.css';
+
 export default class Checkout extends Component {
   constructor() {
     super();
@@ -63,124 +63,130 @@ export default class Checkout extends Component {
     const { verify } = this.state;
     const { CartItemsList } = this.props;
     return (
-      <div>
+      <div className="container-checkout">
         <h2 className="finalization">Finalizacao de compra</h2>
         <form className="checkout-form">
-          <label
-            htmlFor="Nome"
-          >
-            Nome Completo
-            <input
-              data-testid="checkout-fullname"
-              name="Nome"
-              onChange={ this.onInputChange }
-              type="text"
-            />
-          </label>
-          <label
-            htmlFor="Email"
-          >
-            Email
-            <input
-              data-testid="checkout-email"
-              name="Email"
-              onChange={ this.onInputChange }
-              type="text"
-            />
-          </label>
-          <label
-            htmlFor="CPF"
-          >
-            CPF
-            <input
-              data-testid="checkout-cpf"
-              name="CPF"
-              onChange={ this.onInputChange }
-              type="text"
-            />
-          </label>
-          <label
-            htmlFor="Telefone"
-          >
-            Telefone
-            <input
-              data-testid="checkout-phone"
-              name="Telefone"
-              onChange={ this.onInputChange }
-              type="text"
-            />
-          </label>
-          <label
-            htmlFor="CEP"
-          >
-            CEP
-            <input
-              data-testid="checkout-cep"
-              name="CEP"
-              onChange={ this.onInputChange }
-              type="text"
-            />
-          </label>
-          <label
-            htmlFor="Endereco"
-          >
-            Endereço
-            <input
-              data-testid="checkout-address"
-              name="Endereco"
-              onChange={ this.onInputChange }
-              type="text"
-            />
-          </label>
-          <label
-            htmlFor="Boleto"
-          >
-            <input
-              data-testid="ticket-payment"
-              name="payment"
-              onChange={ this.onInputChange }
-              type="radio"
-              value="Boleto"
-            />
-            Boleto
-          </label>
-          <label
-            htmlFor="Visa"
-          >
-            <input
-              data-testid="visa-payment"
-              name="payment"
-              onChange={ this.onInputChange }
-              type="radio"
-              value="Visa"
-            />
-            Visa
-          </label>
-          <label
-            htmlFor="MasterCard"
-          >
-            <input
-              data-testid="master-payment"
-              name="payment"
-              onChange={ this.onInputChange }
-              type="radio"
-              value="MasterCard"
-            />
-            MasterCard
-          </label>
-          <label
-            htmlFor="Elo"
-          >
-            <input
-              data-testid="elo-payment"
-              name="payment"
-              onChange={ this.onInputChange }
-              type="radio"
-              value="Elo"
-            />
-            Elo
-          </label>
-
+          <div className="container-primary-information">
+            <label
+              htmlFor="Nome"
+            >
+              Nome Completo:
+              <input
+                data-testid="checkout-fullname"
+                name="Nome"
+                onChange={ this.onInputChange }
+                type="text"
+              />
+            </label>
+            <label
+              htmlFor="Email"
+            >
+              Email:
+              <input
+                data-testid="checkout-email"
+                name="Email"
+                onChange={ this.onInputChange }
+                type="text"
+              />
+            </label>
+            <label
+              htmlFor="CPF"
+            >
+              CPF:
+              <input
+                data-testid="checkout-cpf"
+                name="CPF"
+                onChange={ this.onInputChange }
+                type="text"
+              />
+            </label>
+          </div>
+          <div className="second-information">
+            <label
+              htmlFor="Telefone"
+            >
+              Telefone:
+              <input
+                data-testid="checkout-phone"
+                name="Telefone"
+                onChange={ this.onInputChange }
+                type="text"
+              />
+            </label>
+            <label
+              htmlFor="CEP"
+            >
+              CEP:
+              <input
+                data-testid="checkout-cep"
+                name="CEP"
+                onChange={ this.onInputChange }
+                type="text"
+              />
+            </label>
+            <label
+              htmlFor="Endereco"
+            >
+              Endereço:
+              <input
+                data-testid="checkout-address"
+                name="Endereco"
+                onChange={ this.onInputChange }
+                type="text"
+              />
+            </label>
+          </div>
+          <div className="form-pgto">
+            <p>Forma de Pagamento:</p>
+            <label
+              htmlFor="Boleto"
+            >
+              <input
+                data-testid="ticket-payment"
+                name="payment"
+                onChange={ this.onInputChange }
+                type="radio"
+                value="Boleto"
+              />
+              Boleto
+            </label>
+            <label
+              htmlFor="Visa"
+            >
+              <input
+                data-testid="visa-payment"
+                name="payment"
+                onChange={ this.onInputChange }
+                type="radio"
+                value="Visa"
+              />
+              Visa
+            </label>
+            <label
+              htmlFor="MasterCard"
+            >
+              <input
+                data-testid="master-payment"
+                name="payment"
+                onChange={ this.onInputChange }
+                type="radio"
+                value="MasterCard"
+              />
+              MasterCard
+            </label>
+            <label
+              htmlFor="Elo"
+            >
+              <input
+                data-testid="elo-payment"
+                name="payment"
+                onChange={ this.onInputChange }
+                type="radio"
+                value="Elo"
+              />
+              Elo
+            </label>
+          </div>
           <button
             data-testid="checkout-btn"
             type="submit"
