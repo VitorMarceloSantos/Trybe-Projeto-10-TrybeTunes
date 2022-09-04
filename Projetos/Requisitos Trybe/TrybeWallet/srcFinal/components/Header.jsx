1ef144642a89ad51bdc0af12a39800c3794cecbb
Vitor Marcelo <75284384+VitorMarceloSantos@users.noreply.github.com>
2022-09-04 11:00:53 -0300

    Add files via upload

---
+++ Projetos/Requisitos Trybe/TrybeWallet/srcFinal/components/Header.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+  render() {
+    const { userState, walletState } = this.props;
+    return (
+      <div className="container-header">
+        <section className="container-login">
+          <p data-testid="email-field">
+            Email:
+            {`${userState.email}`}
+          </p>
+        </section>
+        <section className="container-expenses">
+          <div className="container-expenses-total">
+            <p>Despesa Total:</p>
+            <p data-testid="total-field">
+              { (walletState.expenses).length === 0 ? (0).toFixed(2) : (
+                ((walletState.expenses)
+                  .reduce((acc, current) => acc + (current.value * (Number(
+                    current.exchangeRates[current.currency].ask,
+                  ))), 0)).toFixed(2)
+              )}
+            </p>
+          </div>
+          <div className="container-coin-header">
+            <p data-testid="header-currency-field">
+              BRL
+            </p>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  userState: state.user,
+  walletState: state.wallet,
+});
+
+export default connect(mapStateToProps, null)(Header);
+
+Header.propTypes = {
+  userState: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+  }).isRequired,
+  walletState: PropTypes.shape({
+    expenses: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  }).isRequired,
+};
---
+++ Projetos/Requisitos Trybe/TrybeWallet/srcFinal/pages/Login.jsx
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { addUserAction } from '../redux/actions';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isDisable: true,
+    };
+  }
+
+  randleChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    }, () => {
+      const { email, password } = this.state;
+      const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      const quantCharacter = 6;
+      if ((password.length >= quantCharacter) && (regexEmail.test(email))) {
+        console.log('Dados Corretos');
+        this.setState({
+          isDisable: false,
+        });
+      } else {
+        // console.log('Dados Incorretos');
+        this.setState({
+          isDisable: true,
+        });
+      }
+    });
+  };
+
+  formSubmit = (event) => {
+    event.preventDefault();
+    const { history, dispatch } = this.props;
+    const { email } = this.state;
+    dispatch(addUserAction(email)); // deve receber como parâmetro um objeto
+    history.push('/carteira');
+  };
+
+  render() {
+    const { email, password, isDisable } = this.state;
+    return (
+      <section className="container-email">
+        <form>
+          <label htmlFor="email">
+            <p>Email:</p>
+            <input
+              type="email"
+              name="email"
+              data-testid="email-input"
+              value={ email }
+              onChange={ this.randleChange }
+            />
+          </label>
+          <label htmlFor="password">
+            <p>Senha:</p>
+            <input
+              type="password"
+              name="password"
+              data-testid="password-input"
+              value={ password }
+              onChange={ this.randleChange }
+            />
+          </label>
+          <input
+            type="submit"
+            value="Entrar"
+            disabled={ isDisable }
+            onClick={ this.formSubmit }
+          />
+        </form>
+      </section>
+    );
+  }
+}
+
+export default connect()(Login);
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
