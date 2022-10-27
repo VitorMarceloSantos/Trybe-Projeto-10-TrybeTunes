62b77c283227937357580dc55eb24a216b6e270b
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-10-27 11:44:30 -0300

    Cards

--- Projetos/Projeto Modificados/TrybeTunes/src/pages/Home.jsx
+++ Projetos/Projeto Modificados/TrybeTunes/src/pages/Home.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import ButtonUpgrade from '../components/ButtonUpgrade';
 import '../styles/buttonUpgrade.css';
 import '../styles/home.css';
 import asMaisTocadas from '../images/asMaisTocadas.png';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default class Home extends Component {
+  state= {
+    loading: true,
+    arraySearch: [],
+  }
+
+  async componentDidMount() {
+    const resultSearch = await searchAlbumsAPI('Guns"n Roses'); // batendo na API
+    // console.log(resultSearch);
+    if (resultSearch.length !== 0) {
+      const NUMBER_SIX = 6;
+      this.setState({
+        arraySearch: (resultSearch).slice(0, NUMBER_SIX),
+        loading: false,
+      });
+    }
+  }
+
   render() {
+    const { loading, arraySearch } = this.state;
     return (
       <section className="container-home">
         <ButtonUpgrade />
@@ -23,6 +43,30 @@ export default class Home extends Component {
             </div>
           </div>
         </div>
+        {loading ? <p>Carregando</p> : (
+          // <p>{console.log(arraySearch)}</p>
+          <div className="container-geral-cards">
+            <h2 className="title-container-card">Guns N&apos; Roses</h2>
+            <div className="container-carousel-musics">
+              {arraySearch.map((element) => (
+                <div className="container-card-music" key={ element.collectionId }>
+                  <Link
+                    data-testid={ `link-to-album-${element.collectionId}` }
+                    to={ `/album/${element.collectionId}` }
+                    key={ element.collectionId }
+                  >
+                    {/* Informações do artista */}
+                    <img src={ element.artworkUrl100 } alt={ element.collectionId } />
+                    <h3>{element.artistName}</h3>
+                    <h4>{element.collectionName}</h4>
+                  </Link>
+                </div>
+              ))}
+
+            </div>
+          </div>
+
+        )}
       </section>
     );
   }
