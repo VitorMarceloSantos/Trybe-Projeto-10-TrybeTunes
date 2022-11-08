beb1847a353165edec3b426d320a875c030c9345
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-11-07 21:10:36 -0300

    Create FavoriteSongs

--- Projetos/Projeto Modificados/TrybeTunes/src/components/Content.jsx
+++ Projetos/Projeto Modificados/TrybeTunes/src/components/Content.jsx
@@ -4,14 +4,6 @@ import { Route, Switch } from 'react-router-dom';
 import Home from '../pages/Home';
 import '../styles/header.css';
 import '../styles/content.css';
-
-// import Login from '../pages/Login';
-// import Album from '../pages/Album';
-// import Favorites from '../pages/Favorites';
-// import NotFound from '../pages/NotFound';
-// import Profile from '../pages/Profile';
-// import ProfileEdit from '../pages/ProfileEdit';
-// import Search from '../pages/Search';
 import SearchBar from '../pages/SearchBar';
 import YourLibrary from '../pages/YourLibrary';
 import Musics from '../pages/Musics';
@@ -28,13 +20,6 @@ export default class Content extends Component {
               <Route exact path="/search" component={ SearchBar } />
               <Route exact path="/library" component={ YourLibrary } />
               <Route exact path="/album/:id" component={ Musics } />
-              {/* <Route exact path="/" component={ Login } />
-        <Route exact path="/search" component={ Search } />
-        <Route exact path="/album/:id" component={ Album } />
-        <Route exact path="/favorites" component={ Favorites } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/profile/edit" component={ ProfileEdit } />
-        <Route exact component={ NotFound } /> */}
             </Switch>
           </div>
         </section>
