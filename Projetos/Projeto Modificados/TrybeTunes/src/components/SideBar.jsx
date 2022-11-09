c2206b32e1170802dd49b3514ce155b39a0001f3
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-11-08 22:23:29 -0300

    Create Library

--- Projetos/Projeto Modificados/TrybeTunes/src/components/SideBar.jsx
+++ Projetos/Projeto Modificados/TrybeTunes/src/components/SideBar.jsx
@@ -5,26 +5,7 @@ import { SiAddthis } from 'react-icons/si';
 import { Link } from 'react-router-dom';
 import '../styles/sideBar.css';
 
-// import { getUser } from '../services/userAPI';
-// import Loading from './Loading';
-
 class SideBar extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     nameUser: {},
-  //     isLoading: false,
-  //   };
-  // }
-
-  // componentDidMount() { // após renderizar deverá chamar a função com o promise da função getUser
-  //   this.setState({ isLoading: true }, async () => { // utiliza o async pois é uma função assincrona
-  //     this.setState({ nameUser: await getUser() }, () => { // retorna um objeto
-  //       this.setState({ isLoading: false });
-  //     }); // espera a conclusão de getUser(Promise), para executar as demais
-  //   });
-  // }
-
   render() {
     // const { nameUser, isLoading } = this.state;
     return (
@@ -44,7 +25,7 @@ class SideBar extends Component {
           </li>
           <li className="icon-list-sup">
             <SiAddthis className="icon-sideBar-add" />
-            <Link to="/create" className="text-sideBar">Create Playlist</Link>
+            <Link to="/search" className="text-sideBar">Create Playlist</Link>
           </li>
           <li className="icon-list-inf">
             <div className="container-heart">
@@ -54,41 +35,6 @@ class SideBar extends Component {
           </li>
         </ul>
       </nav>
-      // <header data-testid="header-component">
-      //   { isLoading ? <Loading /> : (
-      //     <div>
-      //       <h2>Cabeçalho</h2>
-      //       <h3 data-testid="header-user-name">
-      //         {nameUser.name}
-      //       </h3>
-      //       <ul>
-      //         <li>
-      //           <Link
-      //             data-testid="link-to-search"
-      //             to="/search"
-      //           >
-      //             Pesquisar
-      //           </Link>
-      //         </li>
-      //         <li>
-      //           <Link
-      //             data-testid="link-to-favorites"
-      //             to="/favorites"
-      //           >
-      //             Favoritos
-      //           </Link>
-      //         </li>
-      //         <li>
-      //           <Link
-      //             data-testid="link-to-profile"
-      //             to="/profile"
-      //           >
-      //             Perfil
-      //           </Link>
-      //         </li>
-      //       </ul>
-      //     </div>) }
-      // </header>
     );
   }
 }
