483a33f4692884b8a909022cdef60c6eec676cd4
VitorMarceloSantos <vitor.marcelo2009@hotmail.com>
2022-11-11 15:15:25 -0300

    Exercicio Course

--- Exercicios/Backend/Bloco 04/Aula - 03/Exercicios/src/middleware/auth.js
+++
@@ -1,9 +0,0 @@
-const auth = (req, res, next) => {
-  const authToken = req.headers;
-
-  if(!authToken || authToken.length !== 16) {
-    return res.status(401).json({ message: 'Token inválido!' });
-  }
-}
-
-module.exports = auth;
--- Exercicios/Backend/Bloco 04/Aula - 03/Exercicios/src/utils/createToken.js
+++
@@ -1,9 +0,0 @@
-// Gerando um token aleatorio
-// Exemplo saída: 6242e2ba4dc096d6
-const crypto = require('crypto');
-
-function generateToken() {
-  return crypto.randomBytes(8).toString('hex');
-}
-
-module.exports = generateToken;
